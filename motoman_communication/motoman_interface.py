220277ee0beb7df1a48fc49ce585b84b3631ebab
Joseph Fernandez <joseph.fernandez@nist.gov>
2024-07-24 10:51:30 -0400

    Motoman moving!

---
+++ motoman_communication/motoman_interface.py
@@ -0,0 +1,144 @@
+import rclpy
+import rclpy.duration
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+from rclpy.action import ActionClient
+
+from copy import deepcopy
+
+from sensor_msgs.msg import JointState
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from builtin_interfaces.msg import Duration as DurationMsg
+from industrial_msgs.msg import RobotStatus
+from motoros2_interfaces.srv import StartTrajMode
+from std_srvs.srv import Trigger
+
+class MotomanInterface(Node):
+    def __init__(self):
+        super().__init__('competition_interface')
+        
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        self.joint_state_subscriber_ = self.create_subscription(
+            JointState,
+            '/joint_states',
+            self.joint_state_cb_,
+            qos_profile=qos_profile)
+
+        self.robot_status_subscriber_ = self.create_subscription(
+            RobotStatus,
+            '/robot_status',
+            self.robot_status_cb_,
+            qos_profile=qos_profile)
+        
+        self.start_traj_mode_client_ = self.create_client(
+            StartTrajMode,
+            '/start_traj_mode'
+        )
+        
+        self.stop_traj_mode_client_ = self.create_client(
+            Trigger,
+            '/stop_traj_mode'
+        )
+        
+        self.most_recent_joint_state: JointState = None
+                
+        self._action_client = ActionClient(self, FollowJointTrajectory, "/follow_joint_trajectory")
+
+    def joint_state_cb_(self, msg):
+        self.most_recent_joint_state = msg
+    
+    def robot_status_cb_(self, msg: RobotStatus):
+        if msg.error_codes != 0 and msg.in_error.val == 1:
+            self.get_logger().info(f"Robot status error code: {msg.error_codes}")
+            
+        
+    def start_trajectory_mode_(self):
+        request = StartTrajMode.Request()
+
+        # Send the request
+        future = self.start_traj_mode_client_.call_async(request)
+
+        # Wait for the response
+        while not future.done():
+            pass
+        
+        response: StartTrajMode.Response = future.result()
+        
+        self.get_logger().info(f"Start traj mode result code: {response.result_code}")
+
+    def stop_trajectory_mode_(self):
+        request = Trigger.Request()
+
+        # Send the request
+        future = self.stop_traj_mode_client_.call_async(request)
+
+        # Wait for the response
+        while not future.done():
+            pass
+    
+    def move_robot(self):
+        if self.most_recent_joint_state == None:
+            print("No joint states received yet")
+            return
+        
+        self.get_logger().info(f"Current state: \n\n{self.most_recent_joint_state}\n")
+        
+        new_joint_state = deepcopy(self.most_recent_joint_state)
+        
+        new_joint_state.position[-1] += 0.25
+        
+        self.get_logger().info(f"After state: \n\n{new_joint_state}\n")
+        
+        self._action_client.wait_for_server()
+        
+        new_follow_joint_trajectory = FollowJointTrajectory.Goal()
+        new_follow_joint_trajectory.trajectory = self.generate_joint_trajectory(new_joint_state)
+        
+        self.get_logger().info(f"Goal:\n\n{new_follow_joint_trajectory}\n\n")
+        
+        future = self._action_client.send_goal_async(new_follow_joint_trajectory)
+        
+        while not future.done():
+            pass
+        
+        future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+    
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Error code: {result.error_string}')
+        rclpy.shutdown()
+        
+    def generate_joint_trajectory(self, joint_states: JointState):
+        new_joint_trajectory = JointTrajectory()
+        new_joint_trajectory.joint_names = joint_states.name
+        
+        original_point = JointTrajectoryPoint()
+        original_point.positions = self.most_recent_joint_state.position
+        original_point.velocities = [0.0] * 7
+        original_point.time_from_start = rclpy.duration.Duration(seconds=0).to_msg()
+        new_joint_trajectory.points.append(original_point)
+        
+        new_point = JointTrajectoryPoint()
+        new_point.positions = joint_states.position
+        new_point.velocities = [0.0] * 7
+        new_point.time_from_start = rclpy.duration.Duration(seconds=10).to_msg()
+        new_joint_trajectory.points.append(new_point)
+        
+        return new_joint_trajectory
---
+++ nodes/motoman_interface_node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+
+from motoman_communication.motoman_interface import MotomanInterface
+from time import sleep
+import subprocess
+import threading
+
+def main(args = None):
+    rclpy.init(args = args)
+    motoman_interface = MotomanInterface()
+    
+    executor = MultiThreadedExecutor()
+    executor.add_node(motoman_interface)
+    spin_thread = threading.Thread(target=executor.spin)
+    spin_thread.start()
+    
+    sleep(1)
+    motoman_interface.start_trajectory_mode_()
+    sleep(5)
+    motoman_interface.move_robot()
+    sleep(25)
+    motoman_interface.stop_trajectory_mode_()
+    sleep(20)
+    
+    motoman_interface.destroy_node()
+    rclpy.shutdown()
+
+    
+if __name__ == "__main__":
+    main()
